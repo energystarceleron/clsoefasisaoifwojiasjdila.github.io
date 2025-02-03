efa8026d162658fd331cf40d629b454b47775ee2
energystarceleron <yantaizheng11@gmail.com>
2025-02-03 17:31:15 +0000

    Update find.js

--- Extensions/find.js
+++ Extensions/find.js
@@ -1,15 +1,1 @@
-(function () {
 
-
-  /* REGISTER */
-
-  SWAM.registerExtension ({
-    name: '-UNSWAM-PING',
-    id: 'fabiospampinato.hitCircles',
-    description: 'A theme that -UNSWAM-PING.',
-    version: '1.0.0',
-    author: 'gulf',
-    themes: [HitCirclesTheme, HitCirclesGridTheme]
-  });
-
-}());
